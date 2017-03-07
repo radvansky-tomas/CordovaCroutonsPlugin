1565bc82dd464262ce1eb6d1975b784d6251b526
Tomas Radvansky <tomas.radvansky@6pmplc.com>
2017-03-07 20:45:37 +0800

    iOS Final

--- src/ios/CordovaCroutonsPlugin.swift
+++ src/ios/CordovaCroutonsPlugin.swift
@@ -1,25 +1,37 @@
 import UIKit
+import SwiftMessages
 
 @objc(CordovaCroutonsPlugin) class CordovaCroutonsPlugin : CDVPlugin {
     @objc(showAlert:)
     func showAlert(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK
-        )
-        //Disable interaction
-        SVProgressHUD.setDefaultMaskType(.black)
-
         let msg = command.arguments[0] as? String ?? ""
 
-        if msg.characters.count == 0{
-            SVProgressHUD.show()
+        if msg.characters.count != 0{
+            // Instantiate a message view from the provided card view layout. SwiftMessages searches for nib
+            // files in the main bundle first, so you can easily copy them into your project and make changes.
+            let view = MessageView.viewFromNib(layout: .StatusLine)
 
-        }
-        else
-        {
-            SVProgressHUD.show(withStatus: msg)
+            // Theme message elements with the warning style.
+            view.configureTheme(.error)
+
+            // Add a drop shadow.
+            view.configureDropShadow()
+
+            view.configureContent(body: msg)
+
+            // Show the message.
+            SwiftMessages.show(view: view)
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
+            return
         }
 
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Invalid Message")
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
@@ -28,23 +40,34 @@ import UIKit
 
     @objc(showConfirm:)
     func showConfirm(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK
-        )
-        //Disable interaction
-        SVProgressHUD.setDefaultMaskType(.black)
-
         let msg = command.arguments[0] as? String ?? ""
 
-        if msg.characters.count == 0{
-            SVProgressHUD.show()
+        if msg.characters.count != 0{
+            // Instantiate a message view from the provided card view layout. SwiftMessages searches for nib
+            // files in the main bundle first, so you can easily copy them into your project and make changes.
+            let view = MessageView.viewFromNib(layout: .StatusLine)
 
-        }
-        else
-        {
-            SVProgressHUD.show(withStatus: msg)
+            // Theme message elements with the warning style.
+            view.configureTheme(.success)
+
+            // Add a drop shadow.
+            view.configureDropShadow()
+
+            view.configureContent(body: msg)
+
+            // Show the message.
+            SwiftMessages.show(view: view)
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
+            return
         }
 
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Invalid Message")
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
@@ -53,23 +76,35 @@ import UIKit
 
     @objc(showInfo:)
     func showInfo(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_OK
-        )
-        //Disable interaction
-        SVProgressHUD.setDefaultMaskType(.black)
 
         let msg = command.arguments[0] as? String ?? ""
 
-        if msg.characters.count == 0{
-            SVProgressHUD.show()
+        if msg.characters.count != 0{
+            // Instantiate a message view from the provided card view layout. SwiftMessages searches for nib
+            // files in the main bundle first, so you can easily copy them into your project and make changes.
+            let view = MessageView.viewFromNib(layout: .StatusLine)
 
-        }
-        else
-        {
-            SVProgressHUD.show(withStatus: msg)
+            // Theme message elements with the warning style.
+            view.configureTheme(.info)
+
+            // Add a drop shadow.
+            view.configureDropShadow()
+
+            view.configureContent(body: msg)
+
+            // Show the message.
+            SwiftMessages.show(view: view)
+            let pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK
+            )
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
+            return
         }
 
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Invalid Message")
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
@@ -82,7 +117,8 @@ import UIKit
             status: CDVCommandStatus_OK
         )
 
-        SVProgressHUD.dismiss()
+        // Or hide the current message and clear the queue.
+        SwiftMessages.hideAll()
 
         self.commandDelegate!.send(
             pluginResult,
